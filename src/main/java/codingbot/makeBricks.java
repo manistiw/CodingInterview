7653a67134cc0020bf3adaa56ee94da950235e07
manish.tiwari <tiwari.manish085@gmail.com>
2025-03-04 12:36:10 +0100

    added makebricks

---
+++ src/main/java/codingbot/makeBricks.java
@@ -0,0 +1,17 @@
+package codingbot;
+/*
+We want to make a row of bricks that is goal inches long.
+We have a number of small bricks (1 inch each) and big bricks (5 inches each).
+Return true if it is possible to make the goal by choosing from the given bricks
+ */
+public class makeBricks {
+    public static void main(String args[]){
+        System.out.println(makeBricks(3,1,8));
+        System.out.println(makeBricks(3,1,9));
+        System.out.println(makeBricks(3,2,10));
+
+    }
+    public static boolean makeBricks(int small, int big,int goal){
+        return small*1+big*5>=goal?true:false;
+    }
+}
