6226c08a415aa8267557b5d090ed9f446de44432
manish.tiwari <tiwari.manish085@gmail.com>
2025-03-05 12:53:47 +0100

    solved NoTeenSum

---
+++ src/main/java/codingbot/NoTeenSum.java
@@ -0,0 +1,17 @@
+package codingbot;
+
+public class NoTeenSum {
+    public int noTeenSum(int a, int b, int c) {
+        return fixTeen(a)+fixTeen(b)+fixTeen(c);
+
+    }
+    public int fixTeen(int n){
+        if(n>=13 && n<=19 ){
+            if(n==15 || n==16){
+                return n;
+            }
+            return 0;
+        }
+        return n;
+    }
+}
